9f14e82ddfab88050b645abaf3a67785ffebf656
nguyenkhactiem2k1 <nguyenkhactiem2k1@gmail.com>
2023-04-18 20:05:35 +0700

    up

---
+++ MISA.WEB08.AMIS.API/Controllers/BranchsController.cs
@@ -0,0 +1,33 @@
+﻿using MISA.WEB08.AMIS.Common.Entities;
+using MISA.WEB08.AMIS.BL;
+using Microsoft.AspNetCore.Authorization;
+
+namespace MISA.WEB08.AMIS.API.Controllers
+{
+    /// <summary>
+    /// API dữ liệu với bảng unit
+    /// </summary>
+    /// Created by : Nguyễn Khắc Tiềm (21/09/2022)
+    [Authorize]
+    public class BranchsController : BasesController<Branch>
+    {
+        #region Field
+
+        private IBranchBL _unitBL;
+
+        #endregion
+
+        #region Contructor
+
+        public BranchsController(IBranchBL unitBL) : base(unitBL)
+        {
+            _unitBL = unitBL;
+        }
+
+        #endregion
+
+        #region Method
+
+        #endregion
+    }
+}
---
+++ MISA.WEB08.AMIS.Common/Enums/Enums.cs
@@ -0,0 +1,47 @@
+﻿
+namespace MISA.WEB08.AMIS.Common.Enums
+{
+    /// <summary>
+    /// Giới tính 
+    /// Enum giới tính của thực thể người, 3 loại nam, nữ, khác
+    /// </summary>
+    /// Create by: Nguyễn Khắc Tiềm (21/09/2022)
+    public enum Gender:int
+    {
+        /// <summary>
+        /// nam
+        /// Created by : Khắc Tiềm 21.09.2022
+        /// </summary>
+        Male = 0,
+
+        /// <summary>
+        /// nữ
+        /// Created by : Khắc Tiềm 21.09.2022
+        /// </summary>
+        Female = 1,
+
+        /// <summary>
+        /// khác
+        /// Created by : Khắc Tiềm 21.09.2022
+        /// </summary>
+        Other = 2,
+    }
+
+
+    /// <summary>
+    /// Kiểu join bảng
+    /// </summary>
+    /// Create by: Nguyễn Khắc Tiềm (21/09/2022)
+    public enum TypeJoin: int
+    {
+        /// <summary>
+        /// Kiểu inner join
+        /// </summary>
+        InnerJoin = 1,
+
+        /// <summary>
+        /// Kiểu left join
+        /// </summary>
+        LeftJoin = 2,
+    }
+}
